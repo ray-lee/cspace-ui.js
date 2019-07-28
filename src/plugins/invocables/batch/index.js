f8ee3dcc4a57f3f1b6febd5dc6e8c8298164fde4
Ray Lee <ray.lee@lyrasis.org>
2019-07-27 20:32:23 -0700

    DRYD-701: Configure parameter UI for inventory status batch job.

--- src/plugins/invocables/batch/index.js
+++ src/plugins/invocables/batch/index.js
@@ -1,5 +1,7 @@
 import MergeAuthorityItemsBatchJob from './org.collectionspace.services.batch.nuxeo.MergeAuthorityItemsBatchJob';
+import updateInventoryStatusBatchJob from './org.collectionspace.services.batch.nuxeo.UpdateInventoryStatusBatchJob';
 
 export default [
   MergeAuthorityItemsBatchJob,
+  updateInventoryStatusBatchJob,
 ];
